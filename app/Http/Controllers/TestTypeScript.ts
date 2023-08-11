faa4964361fd446547b099639da5a22173e6fb15
carlospicpay <carlos.fernandes@picpay.com>
2023-08-11 20:56:50 -0300

    corretion

--- app/Http/Controllers/TestTypeScript.ts
+++ app/Http/Controllers/TestTypeScript.ts
@@ -1,6 +1,11 @@
 module.exports =  class TestTypeScript{
 
     test(){
-        return "Aplicação com ts";
+        let tabua;
+        tabua = [];
+        for(let i = 1;i<=10;i ++){            
+            tabua.push("5 X "+i+" = "+(5*i));
+        }
+        return tabua;
     } 
 }
